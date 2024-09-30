7621105a319a121c1500660226bcb108dc99bd8c
Juan Caminal <juanigcaminal@gmail.com>
2024-09-30 13:13:57 -0300

    fix: Se descarga bien el archivo si tiene datos, y en caso de estar vacio se cancela la descarga

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ def main():
     # start_date = "2024-08-01"
     # end_date = "2024-08-22"
     
-    start_date = "2024-08-23"
-    end_date = "2024-08-31"
+    # start_date = "2024-08-23"
+    # end_date = "2024-08-31"
     
-    # start_date = yesterday.strftime("%Y-%m-%d")
-    # end_date = yesterday.strftime("%Y-%m-%d")
+    # Date without data:
+    # start_date = "2024-09-28"
+    # end_date = "2024-09-28"
+    
+    start_date = yesterday.strftime("%Y-%m-%d")
+    end_date = yesterday.strftime("%Y-%m-%d")
     
     with yaspin(text="Downloading csv file...", color="yellow") as spinner:
         with sync_playwright() as playwright:
             run(playwright, start_date, end_date, CSV_FILE_NAME, invisible=False)
         spinner.ok("✔️  Download completed!")
-        
-    format_csv.format_csv(CSV_FILE_NAME)
 
 if __name__ == "__main__":
     main()
