9a452fa63f7fe52b34017f618b0f208dc27bc283
Juan Caminal <juanigcaminal@gmail.com>
2024-10-07 14:15:21 -0300

    refactor: se ordeno y separo mejor la logica interna de los archivos services y main. refactor: se organizo mejor la estructura del proyecto

--- main.py
+++ main.py
@@ -1,35 +1,65 @@
 import datetime
-from download import run
+from services.csv_downloader import run
 from playwright.sync_api import sync_playwright
 from yaspin import yaspin
-import format_csv
 
 def main():
     CSV_FILE_NAME = "temporary_file.csv"
-    today = datetime.date.today()
-    one_day = datetime.timedelta(days=1)
-    yesterday = today - one_day
-
+    user = "hmcparker@diblogins.com"
+    password = "eds!WH7#82eLxKmF"
     
-    # Previous dates asked for
-    # start_date = "2024-08-01"
-    # end_date = "2024-08-22"
+    dates = {"start_date":"", "end_date":""}
     
-    # start_date = "2024-08-23"
-    # end_date = "2024-08-31"
+    yesterday = get_yesterday()
     
-    # Date without data:
-    # start_date = "2024-09-28"
-    # end_date = "2024-09-28"
+    # dates = set_yesterday_dates(dates, yesterday)
     
-    start_date = yesterday.strftime("%Y-%m-%d")
-    end_date = yesterday.strftime("%Y-%m-%d")
+    # Variables for testing with and without data
+    dates = set_dates_with_data(dates)
+    # dates = set_dates_without_data(dates)
+    
+    start_date = dates["start_date"]
+    end_date = dates["end_date"]
     
     with yaspin(text="Downloading csv file...", color="yellow") as spinner:
         with sync_playwright() as playwright:
-            run(playwright, start_date, end_date, CSV_FILE_NAME, invisible=False)
+            run(playwright, user, password, start_date, end_date, CSV_FILE_NAME, invisible=False)
         spinner.ok("✔️  Download completed!")
 
+def get_yesterday():
+    today = datetime.date.today()
+    one_day = datetime.timedelta(days=1)
+    return today - one_day
+
+# Set the dates from the csv file that will be downloaded to the previous day date
+def set_yesterday_dates(dates, yesterday):
+    dates["start_date"] = yesterday.strftime("%Y-%m-%d")
+    dates["end_date"] = yesterday.strftime("%Y-%m-%d")
+    
+    return dates
+
+# Set the dates from the csv file that will be downloaded with dates that have data
+# (Choose one of them)
+def set_dates_with_data(dates):
+     # First dates asked for
+    dates["start_date"] = "2024-08-01"
+    dates["end_date"] = "2024-08-22"
+    
+    # Second dates asked for
+    # dates["start_date"] = "2024-08-23"
+    # dates["end_date"] = "2024-08-31"
+    
+    return dates
+
+# Set the dates from the csv file that will be downloaded with dates that don't have data
+def set_dates_without_data(dates):
+    
+    dates["start_date"] = "2024-09-28"
+    dates["end_date"] = "2024-09-28"
+    
+    return dates
+    
+    
 
 if __name__ == "__main__":
     main()
