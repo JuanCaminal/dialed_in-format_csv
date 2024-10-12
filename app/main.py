167246de7f2a598688f66c6ee0ca3114f0f16256
Juan Caminal <juanigcaminal@gmail.com>
2024-10-12 13:24:30 -0300

    Se agrego una funcion de entrada para el protocolo pub/sub

--- app/main.py
+++ app/main.py
@@ -3,6 +3,14 @@ from services.csv_downloader import run
 from playwright.sync_api import sync_playwright
 from yaspin import yaspin
 import json
+import base64
+import functions_framework
+
+# Triggered from a message on a Cloud Pub/Sub topic.
+@functions_framework.cloud_event
+def hello_pubsub(cloud_event):
+    # Print out the data from Pub/Sub, to prove that it worked
+    print(base64.b64decode(cloud_event.data["message"]["data"]))
 
 def main():
     CSV_FILE_NAME = "temporary_file.csv"
